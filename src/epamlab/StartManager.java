86f9e8f8ffa00fa61497e2a71ec5a7a3161ea14c
mirror <mironosk@gmail.com>
2016-04-08 07:03:06 +0300

    new project

---
+++ src/epamlab/StartManager.java
@@ -0,0 +1,12 @@
+package epamlab;
+
+public class StartManager {
+	
+	public static volatile boolean startFlag = false;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
